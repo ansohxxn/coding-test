8c89650654fc324aae4284952e1dc3a62cd5effa
DESKTOP-S1KVEJK\소현제현 <lobreamer@daum.net>
2021-03-24 20:46:24 +0900

    예산

---
+++ 프로그래머스/Level_1/43.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <stdio.h>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(vector<int> d, int budget) {
+    int answer = 0;
+    int sum = 0;
+    
+    sort(d.begin(), d.end());
+    
+    for(int i = 0; i < d.size(); i++)
+    {
+        if (d[i] + sum > budget)
+            break;
+        sum += d[i];
+        answer++;
+    }
+    
+    return answer;
+}
