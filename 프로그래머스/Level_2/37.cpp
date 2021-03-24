8fc753c0162e8e15a2516d0f4b2957781be5cbc3
DESKTOP-S1KVEJK\소현제현 <lobreamer@daum.net>
2021-03-25 01:18:13 +0900

    최솟값 만들기

---
+++ 프로그래머스/Level_2/37.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int solution(vector<int> A, vector<int> B)
+{
+    int answer = 0;
+
+    sort(A.begin(), A.end());
+    sort(B.begin(), B.end(), greater<int>());
+
+    for (int i = 0; i < A.size(); i++)
+        answer += A[i] * B[i];
+
+    return answer;
+}
