50f82e9c6e37b0b546733ee88df37fd429f12590
DESKTOP-S1KVEJK\소현제현 <lobreamer@daum.net>
2021-03-24 20:41:01 +0900

    핸드폰 번호 가리기

---
+++ 프로그래머스/Level_1/38.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string phone_number) {
+    
+    for(int i = 0; i < phone_number.size() - 4; i++)
+        phone_number[i] = '*';
+    
+    return phone_number;
+}
