3d114b561420afaa3cf55c908d6b46c930507594
DESKTOP-S1KVEJK\소현제현 <lobreamer@daum.net>
2021-03-25 01:25:54 +0900

    점프와 순간 이동

---
+++ 프로그래머스/Level_2/45.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int solution(int n)
+{
+    int ans = 0;
+    
+    while(n > 0){
+        if (n % 2 == 1)
+            ans++;
+        n /= 2;
+    }
+
+    return ans;
+}
