e8bca78d6183082849cb104952022ef7bc75bf21
DESKTOP-S1KVEJK\소현제현 <lobreamer@daum.net>
2021-03-24 20:41:20 +0900

    행렬의 덧셈

---
+++ 프로그래머스/Level_1/39.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<vector<int>> solution(vector<vector<int>> arr1, vector<vector<int>> arr2) {
+    vector<vector<int>> answer;
+    
+    for(int i = 0; i < arr1.size(); i++)
+    {
+        vector<int> temp(arr1[i].size());
+        answer.push_back(temp);
+        for (int j = 0; j < arr1[i].size(); j++)
+            answer[i][j] = arr1[i][j] + arr2[i][j];
+    }
+    
+    return answer;
+}
