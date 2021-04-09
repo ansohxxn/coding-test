e05cad2d7f259d11c66ab8aa87f2b55f53ac4841
DESKTOP-S1KVEJK\소현제현 <lobreamer@daum.net>
2021-04-09 10:41:24 +0900

    N 찍기

---
+++ 백준/2741.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    
+    for(int i = 1; i <= n; i++)
+    {
+        cout << i << '\n';
+    }
+    
+    return 0;
+}
