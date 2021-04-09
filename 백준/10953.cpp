76ae085ab66470dc9c40737e40a9e03c0464e3ba
DESKTOP-S1KVEJK\소현제현 <lobreamer@daum.net>
2021-04-09 11:35:45 +0900

    A+B - 6

---
+++ 백준/10953.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+using namespace std;
+
+int main()
+{
+   int n, a, b;
+   scanf("%d", &n);
+   
+   for(int i = 0; i < n; i++)
+   {
+       scanf("%d,%d", &a, &b);
+       printf("%d\n", a + b);
+   }
+   return 0;
+}
