798574bd84ad3ef99286032ac624e37cf76377eb
DESKTOP-S1KVEJK\소현제현 <lobreamer@daum.net>
2021-04-12 01:09:27 +0900

    1070 : [기초-조건/선택실행구조] 월 입력받아 계절 출력하기

---
+++ 코드업/C언어 기초 100제/1070.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    
+    switch(n)
+    {
+        case 12: case 1: case 2:
+            printf("winter");
+            break;
+        case 3: case 4: case 5:
+            printf("spring");
+            break;
+        case 6: case 7: case 8:
+            printf("summer");
+            break;
+        case 9: case 10: case 11:
+            printf("fall");
+            break;
+    }
+}
