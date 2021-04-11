8e133e920251d8747b6f4da6582585258f281f0c
DESKTOP-S1KVEJK\소현제현 <lobreamer@daum.net>
2021-04-12 01:24:24 +0900

    1072 : [기초-반복실행구조] 정수 입력받아 계속 출력하기

---
+++ 코드업/C언어 기초 100제/1072.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, a; 
+    scanf("%d", &n);
+    
+    for(int i = 0; i < n; i++)
+    {
+        scanf("%d", &a);
+        printf("%d\n", a);
+    }
+}
