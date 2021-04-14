a64886c613b78d388eb64eac2c40faec12a26b8b
DESKTOP-S1KVEJK\소현제현 <lobreamer@daum.net>
2021-04-15 01:13:12 +0900

    전화번호 목록

---
+++ 백준/5052-1.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <deque>
+#include <queue>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <unordered_set>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int t, n;
+    cin >> t;
+    
+    for (int i = 0; i < t; ++i) {
+        cin >> n;
+        vector<string> arr(n);
+        for (int j = 0; j < n; ++j)
+            cin >> arr[j];
+        
+        sort(arr.begin(), arr.end());
+        unordered_set<string> hash_set;
+        bool flag = false;
+        for (int j = 0; j < n; ++j) {
+            string str = "";
+            for (int k = 0; k < arr[j].length(); ++k) {
+                str += arr[j][k];
+                if (hash_set.find(str) != hash_set.end()) {
+                    flag = true;
+                    cout << "NO" << '\n';
+                    break;
+                }
+            }
+            if (flag) break;
+            hash_set.insert(str);
+        }
+        if (!flag) cout << "YES" << '\n';
+    }
+}
