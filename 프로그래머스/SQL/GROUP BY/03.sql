d6254c36cdb1c2c20dfc639f979f6a606f4665d6
DESKTOP-S1KVEJK\소현제현 <lobreamer@daum.net>
2021-05-10 16:23:47 +0900

    입양 시각 구하기(1)

--- 프로그래머스/SQL/GROUP BY/03.sql
+++ 프로그래머스/SQL/GROUP BY/03.sql
@@ -3,8 +3,3 @@ FROM ANIMAL_OUTS
 WHERE HOUR(DATETIME) >= 9 AND HOUR(DATETIME) <= 19
 GROUP BY HOUR(DATETIME)
 ORDER BY HOUR(DATETIME)
-
--- GROUP BY 와 ORDER BY 에도 집계함수 쓸 수 있음
--- 집계함수는 결과이기 때문에.
-
--- 값이 같은 행들끼리 묶어준다 ! ! !
