976563188b686d36cf07c3237d0a60dc9dae88e9
DESKTOP-S1KVEJK\소현제현 <lobreamer@daum.net>
2021-05-10 16:35:00 +0900

    입양 시각 구하기 (2)

--- 프로그래머스/SQL/GROUP BY/04.sql
+++ 프로그래머스/SQL/GROUP BY/04.sql
@@ -5,11 +5,3 @@ SELECT @HOUR := @HOUR + 1,
         WHERE HOUR(DATETIME) = @HOUR)
 FROM ANIMAL_OUTS
 WHERE @HOUR < 23
-
-
--- 변수 선언 
-  -- SET @변수이름 = 값;
--- 변수 대입
-  -- SET @변수이름 := 값
-
--- 변수도 SELECT 로 열 이름으로서 출력 가능. 말그대로 변수임 변수!
