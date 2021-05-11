ed78e8896d240275db419d6b1ea24b70fcfb93bb
DESKTOP-S1KVEJK\소현제현 <lobreamer@daum.net>
2021-05-11 23:53:39 +0900

    우유와 요거트가 담긴 장바구니

--- 프로그래머스/SQL/01-2.sql
+++ 프로그래머스/SQL/01-2.sql
@@ -1,6 +1,3 @@
--- 우유와 요거트를 동시에 구입한 장바구니의 아이디를 조회하는 SQL 문을 작성해주세요. 
--- 이때 결과는 장바구니의 아이디 순으로 나와야 합니다.
-
 SELECT P.CART_ID 
 FROM CART_PRODUCTS P JOIN (SELECT CART_ID FROM CART_PRODUCTS WHERE NAME="Yogurt") YO 
     ON P.CART_ID=YO.CART_ID 
