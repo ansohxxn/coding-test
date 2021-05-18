48894fc9ae71549c1a83454acd51a9e3f886b5b9
DESKTOP-S1KVEJK\소현제현 <lobreamer@daum.net>
2021-05-19 02:28:11 +0900

    2개 이하로 다른 비트

--- 프로그래머스/Level_2/62.cpp
+++ 프로그래머스/Level_2/62.cpp
@@ -9,16 +9,12 @@ vector<long long> solution(vector<long long> numbers) {
         if (numbers[i] % 2 == 0)
             answer.push_back(numbers[i] + 1);
         else {
-            if ((numbers[i] & 1) & (numbers[i] & 2) == 0)
-                answer.push_back(numbers[i] + 1);
-            else {
-                long long count = 1;
-                while (true) {
-                    if ((numbers[i] & count) == 0) break;
-                    count *= 2;
-                }
-                answer.push_back(numbers[i] + count / 2);
+            long long count = 1;
+            while (true) {
+                if ((numbers[i] & count) == 0) break;
+                count *= 2;
             }
+            answer.push_back(numbers[i] + count / 2);
         }
     }
     return answer;
